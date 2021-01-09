a510416c8f05e67b6e9778eac877e43e1215826e
cyberroot294 <nabilumarsyarif294@gmail.com>
2021-01-09 13:38:29 +0700

    add Populate Array to fill array with random numbers

---
+++ SortingAlgorithm/PopulateArray.java
@@ -0,0 +1,9 @@
+package SortingAlgorithm;
+
+public class PopulateArray {
+    public static void populateArray(int range, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) Math.floor(Math.random() * range + 1);
+        }
+    }
+}
